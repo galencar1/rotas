76004c18f6872ed791945182471221a343307778
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-04-22 11:25:11 -0300

    teste rotas - erro angular2-materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import 'materialize-css'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
